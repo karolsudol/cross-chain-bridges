5f272a93ee848e91ac174720dbe5f4f152d68afa
elcomendante <karol@finsane.com>
2022-11-07 18:39:01 +0000

    bridge

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -8,6 +8,8 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./TokenERC20.sol";
 
 interface IBridge {
     function deposit(
@@ -32,14 +34,15 @@ interface IBridge {
     function setChain(uint256 chainId) external;
 }
 
-contract Bridge is Ownable {
+contract Bridge is Ownable, ReentrancyGuard {
     using ECDSA for bytes32;
 
     address private validator;
 
-    mapping(uint256 => bool) public chains;
-    mapping(string => bool) public tokens;
-    mapping(string => address) public tokenAddresses;
+    uint256 public chainID;
+    address token;
+    string tokenSymbol;
+    mapping(string => bool) public tokenSymbols;
     mapping(bytes => bool) public signatures;
 
     event SwapInitialized(
@@ -48,10 +51,70 @@ contract Bridge is Ownable {
         uint256 amount,
         uint256 nonce,
         uint256 chainId,
-        string _symbol
+        string symbol
     );
 
-    constructor(address _validator) {
+    constructor(
+        address _validator,
+        address _token,
+        uint256 _chainID
+    ) {
         validator = _validator;
+        chainID = _chainID;
+        token = _token;
+        tokenSymbol = TokenERC20(_token).symbol();
+        tokenSymbols[tokenSymbol] = true;
+    }
+
+    function swap(
+        address _to,
+        uint256 _amount,
+        uint256 _nonce,
+        uint256 _chainID,
+        string memory _tokenSymbol
+    ) external {
+        require(tokenSymbols[tokenSymbol] == true, "non supported erc20 token");
+        require(chainID == _chainID, "non supported chain");
+
+        TokenERC20(token).burn(msg.sender, _amount);
+
+        emit SwapInitialized(
+            msg.sender,
+            _to,
+            _amount,
+            _nonce,
+            _chainID,
+            _tokenSymbol
+        );
+    }
+
+    function redeem(
+        address from,
+        address to,
+        uint256 amount,
+        uint256 nonce,
+        uint256 _chainID,
+        string memory _symbol,
+        bytes calldata signature
+    ) external {
+        require(tokenSymbols[tokenSymbol] == true, "non supported erc20 token");
+        require(chainID == _chainID, "non supported chain");
+        require(!signatures[signature], "no reentrancy");
+
+        bytes32 message = keccak256(
+            abi.encodePacked(from, to, amount, nonce, _chainID, _symbol)
+        );
+        require(_verify(message, signature, validator), "invalid signature");
+        signatures[signature] = true;
+
+        TokenERC20(token).mint(to, amount);
+    }
+
+    function _verify(
+        bytes32 data,
+        bytes calldata signature,
+        address account
+    ) internal pure returns (bool) {
+        return data.toEthSignedMessageHash().recover(signature) == account;
     }
 }
